ac267c7acf449ae350f4478937e8969d79fa8f80
nzmpi <nizami.abdulov@gmail.com>
2024-04-26 02:29:24 +0400

    init

---
+++ test/TransientLock.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+import "../src/TransientLock.sol";
+
+contract TestTransientLock is Test {
+    function test() public {
+
+    }
+}
